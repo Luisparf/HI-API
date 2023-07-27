d210d255915a61fa997fa3227fdb115da81b2a43
luisparf <luispauloaraujoafonso@gmail.com>
2023-07-27 16:37:49 -0300

    finish

--- src/Plano.php
+++ src/Plano.php
@@ -2,9 +2,9 @@
 
 namespace Luis\HiApi;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 class Plano
 {
